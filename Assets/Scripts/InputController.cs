7f95e149df2cb31eda822f56d55fc8c21bb9ccc2
zwarn <Michael.Budahn@gmail.com>
2015-07-17 19:03:19 +0200

    initial import

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	public string rockButton;
+	public string paperButton;
+	public string stoneButton;
+	public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton (rockButton)) {
+
+		}
+	}
+}
